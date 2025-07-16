4e4fc613e2d19d6b9af9acd23db46d2b8be298fa
surya narayanan <74855435+soor234@users.noreply.github.com>
2025-07-17 01:32:45 +0530

    Create calensedesigh.py

---
+++ calensedesigh.py
@@ -0,0 +1,26 @@
+import calendar
+from types import new_class
+
+
+class calenser:
+    calendar=new_class('Calendar')
+    calender=calender()
+    calender.calendar()
+    for calendar in calender.calendar:
+        calender.formatted=calendar.formatted
+        calender.title=calendar.title
+         calender.description=calendar.description
+        print(calender.formattedlength())
+    while cal.not in textwrap.wrap(calendar.title):
+        calendar.month=new_class(calendar.month)
+        calender.headingline("month")
+    datetime.calender(calender.formattedlength)
+    if(calendar.day_name not in textwrap.wrap(calendar.title)):
+        calendar.day_name=calendar.day_name
+        print(calendar.day_name)
+    calender.sidedropdown= new_class('Sidedropdown')
+if drop:
+    print(calendar.sidedropdown)
+else:
+    print("dropdown not found")
+    
